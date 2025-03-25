c7367d9f27ee6e6228270d966a31b750f8d5521e
주영 최 <juchoi@acewavetech.com>
2025-03-25 10:47:33 +0900

    서버(UDP) 테스트 코드

---
+++ TCP_IP/Server_UDP.py
@@ -0,0 +1,25 @@
+import socket
+
+HOST = '127.0.0.1'
+BIND_PORT = 65333
+SEND_PORT = 65222
+
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as bind_socket:
+    bind_socket.bind((HOST, BIND_PORT))
+    bind_socket.setblocking(False)
+    print("UDP server is up and listening")
+    
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as send_socket:
+        while True:
+            try:
+                data = bind_socket.recvfrom(1024)
+                print(data)
+                msg  = data[0]
+                addr = data[1]
+
+                #msg_from_client = "Hello Server from client"
+                #bytes_to_send = str.encode(msg_from_client)
+                send_socket.sendto(msg, addr)
+            except BlockingIOError:
+                continue
+
